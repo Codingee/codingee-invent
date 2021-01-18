e15f6603c94be1a47840a6920dbc51d0d6b96bf3
Aryan Ganotra <aryanganotra7@gmail.com>
2021-01-19 01:53:15 +0530

    Updates

---
+++ static/js/5.3dab20d1.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpcodingee-developers"]=this["webpackJsonpcodingee-developers"]||[]).push([[5],{122:function(e,c,s){"use strict";s.r(c);var t=s(120),n=s(1),o=(s(0),s(123));c.default=function(e){return Object(n.jsx)(o.a,Object(t.a)(Object(t.a)({},e),{},{src:"/img/svg/".concat(e.src)}))}}}]);
+//# sourceMappingURL=5.3dab20d1.chunk.js.map
